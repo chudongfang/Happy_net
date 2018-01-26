7d79be86b2d8e6d9c492ae63bbda621cd0b312ae
chudongfang <1149669942@qq.com>
2018-01-27 00:20:40 +0800

    reactor

--- net/Poller.cc
+++ net/Poller.cc
@@ -6,4 +6,32 @@
 #include <poll.h>
 using namespace Happy;
 
+Poller::Poller(EventLoop* loop)
+ : ownerLoop_(loop)
+ {
+ }
+
+Poller::~Poller()
+{
+}
+
+Timestap Poller::poll(int timeoutMs, ChannelList * activeChannels)
+{
+    int numEvents = ::poll(&*pollfds_.begin(),pollfds_.size(),timeoutMs);
+    Timestap now(Timestap::now());
+    if(numEvents>0)
+    {
+        cout << numEvents<<" events happended";
+        fillActiveChannels(numEvents,activeChannels);
+    }
+    else if(numEvents == 0 )
+    {
+        cout << "Nothing happended"<<endl;
+    }
+    else 
+    {
+        cout <<"Poller::poll()"<<endl;
+    }
+    return now;
+}
 
