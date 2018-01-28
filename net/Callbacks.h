34f0ff95327d320d1bf2f75357211f7392171c4f
chudongfang <1149669942@qq.com>
2018-01-29 01:10:35 +0800

    reactor

--- net/Callbacks.h
+++ net/Callbacks.h
@@ -2,13 +2,14 @@
 	> File Name: Callbacks.h
 	> Author: 
 	> Mail: 
-	> Created Time: 2018年01月28日 星期日 18时11分15秒
+	> Created Time: 2018年01月29日 星期一 00时50分46秒
  ************************************************************************/
 
 #ifndef _CALLBACKS_H
 #define _CALLBACKS_H
-#include <boost/function.hpp>
-#include <boost/shared_ptr.hpp>
+
+#include<boost/function.hpp>
+#include<boost/shared_ptr.hpp>
 
 namespace Happy
 {
