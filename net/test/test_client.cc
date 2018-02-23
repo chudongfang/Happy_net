daa63c0de9eff59cf636a2bca7b054362faa5c2c
chudongfang <1149669942@qq.com>
2018-02-23 22:26:00 +0800

    reactor_socket

--- net/test/test_client.cc
+++ net/test/test_client.cc
@@ -21,13 +21,14 @@ int main(int argc, char const *argv[])
 	int sockfd = socket(AF_INET,SOCK_STREAM,0);//启动socket
 	struct sockaddr_in addr;
 	addr.sin_family = AF_INET;
-	addr.sin_port = htons(12345);
+	addr.sin_port = htons(12349);
 	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 	if(connect(sockfd,(SA*)&addr,sizeof(addr))==-1){
 	    perror("无法连接到服务器");
 	    printf("客户端启动失败\n");
 	    exit(-1);
 	}
+    //sleep(20);
 	printf("客户端启动成功\n");
     send(sockfd,"lala",5,0) ;
 	return 0;
